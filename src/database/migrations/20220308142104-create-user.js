cf10825f09670b4d8edb2c7154502b70562381fe
Roberval Filho <rslfilho@gmail.com>
2022-03-08 11:57:04 -0300

    feat : migrations de user e post via Sequelize

---
+++ src/database/migrations/20220308142104-create-user.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      displayName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'display_name',
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
