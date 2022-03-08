833918c00bc9a38ab4760646308256a13e70470a
Roberval Filho <rslfilho@gmail.com>
2022-03-08 19:23:51 -0300

    [ PR ] - Setup express (#3)
    * setup : adicionando novos scripts e corrigindo main
    
    * feat : adicionando Router raiz
    
    * feat : adicionando middleware de erro
    
    * feat : adicionando middleware e descritivo do swagger
    
    * feat : adicionando arquivos de configuração e inicialização do express
    
    * fix : retirando scripts adicionados

---
+++ src/api/app.js
@@ -0,0 +1,17 @@
+const cors = require('cors');
+const express = require('express');
+
+const root = require('../routes/root');
+const { error, swagger } = require('../middlewares');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/swagger', swagger.serve, swagger.setup);
+
+app.use(root);
+app.use(error);
+
+module.exports = app;
