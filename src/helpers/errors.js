df5acb5737d6579d8735675fa1b452bd1166630d
Roberval Filho <rslfilho@gmail.com>
2022-03-11 17:19:47 -0300

    feat : atualizando arquivo auxiliar de erros

--- src/helpers/errors.js
+++ src/helpers/errors.js
@@ -4,6 +4,31 @@ const errors = {
     code: 'conflict',
     message: 'Usuário já existe',
   },
+  nameLength: {
+    statusCode: 400,
+    code: 'bad_request',
+    message: '"displayName" length must be at least 8 characters long',
+  },
+  invalidEmail: {
+    statusCode: 400,
+    code: 'bad_request',
+    message: '"email" must be a valid email',
+  },
+  emailRequired: {
+    statusCode: 400,
+    code: 'bad_request',
+    message: '"email" is required',
+  },
+  passwordLength: {
+    statusCode: 400,
+    code: 'bad_request',
+    message: '"password" length must be 6 characters long',
+  },
+  passwordRequired: {
+    statusCode: 400,
+    code: 'bad_request',
+    message: '"password" is required',
+  },
 };
 
 module.exports = errors;
