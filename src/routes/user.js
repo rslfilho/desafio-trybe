9f141c8a9a9c0d0fdc426ba77460671bf297db5b
Roberval Filho <rslfilho@gmail.com>
2022-03-12 18:05:45 -0300

    feat : adicionando rota GET/user

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
 const userController = require('../controllers/user');
-const { validateUser } = require('../middlewares');
+const { validateUser, auth } = require('../middlewares');
 
 const router = express.Router({ mergeParams: true });
 
 router.post('/', validateUser, userController.create);
+router.get('/', auth, userController.getAll);
 
 module.exports = router;
