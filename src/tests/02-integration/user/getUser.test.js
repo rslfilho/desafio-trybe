1cd205084b38f5ab820fa8f9f60d630f86daba63
Roberval Filho <rslfilho@gmail.com>
2022-03-12 17:43:56 -0300

    test : testes de integração da rota GET/user

---
+++ src/tests/02-integration/user/getUser.test.js
@@ -0,0 +1,125 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+
+const app = require('../../../api/app');
+const { User } = require('../../../database/models');
+const { errors, jwt } = require('../../../helpers');
+const userMock = require('../../mocks/user');
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('GET/user', () => {
+  describe('Em caso de erro interno', () => {
+    let response;
+    before(async () => {
+      sinon.stub(jwt, 'validateToken').returns(userMock.created)
+      sinon.stub(User, 'findAll').rejects();
+      response = await chai.request(app)
+        .get('/user')
+        .set('Authorization', userMock.token)
+
+      await jwt.validateToken.restore();
+      await User.findAll.restore();
+    });
+
+    it('retorna o código de status 500', () => {
+      expect(response).to.have.status(500);
+    });
+
+    it('retorna um objeto', () => {
+      expect(response.body).to.be.an('object');
+    });
+
+    it('o objeto possui a propriedade "message"', () => {
+      expect(response.body).to.have.property('message');
+    });
+
+    it('a propriedade "message" possui o texto "Internal Error"', () => {
+      expect(response.body.message).to.be.equal('Internal Error');
+    });
+  });
+
+  describe('Não é possível listar usuários', () => {
+    describe('com token de autenticação ausente', () => {
+      let response;
+      before(async () => {
+        response = await chai.request(app)
+          .get('/user')
+      });
+
+      it('retorna o código de status 401', () => {
+        expect(response).to.have.status(401);
+      });
+
+      it('retorna um objeto', () => {
+        expect(response.body).to.be.an('object');
+      });
+
+      it('o objeto possui a propriedade "message"', () => {
+        expect(response.body).to.have.property('message');
+      });
+
+      it('a propriedade "message" possui o texto "Token não encontrado"', () => {
+        expect(response.body.message).to.be.equal('Token não encontrado');
+      });
+    });
+
+    describe('com um token de autenticação inválido ou expirado', () => {
+      let response;
+      before(async () => {
+        response = await chai.request(app)
+          .get('/user')
+          .set('Authorization', 'tokenIvalido')
+      });
+
+      it('retorna o código de status 401', () => {
+        expect(response).to.have.status(401);
+      });
+
+      it('retorna um objeto', () => {
+        expect(response.body).to.be.an('object');
+      });
+
+      it('o objeto possui a propriedade "message"', () => {
+        expect(response.body).to.have.property('message');
+      });
+
+      it('a propriedade "message" possui o texto "Token expirado ou inválido"', () => {
+        expect(response.body.message).to.be.equal('Token expirado ou inválido');
+      });
+    });
+  });
+
+  describe('É possível listar usuários com token válido', () => {
+    let response;
+      before(async () => {
+        sinon.stub(jwt, 'validateToken').returns(userMock.created)
+        sinon.stub(User, 'findAll').resolves(userMock.userList);
+        response = await chai.request(app)
+        .get('/user')
+        .set('Authorization', userMock.token)
+
+      await jwt.validateToken.restore();
+      await User.findAll.restore();
+      });
+      
+      it('retorna o código de status 200', () => {
+        expect(response).to.have.status(200);
+      });
+
+      it('retorna um array', () => {
+        expect(response.body).to.be.an('array');
+      });
+
+      it('o array possui objetos', () => {
+        expect(response.body[0]).to.be.an('object');
+      })
+
+      it('o array é igual ao esperado', () => {
+        expect(response.body).to.deep.equals(userMock.userList)
+      });
+  });
+});
