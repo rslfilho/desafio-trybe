ee64c3c5041b5b7d3e3906dc0a0151e02fc2e4b5
Roberval Filho <rslfilho@gmail.com>
2022-03-12 10:06:17 -0300

    feat : adicionando rota POST/login

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const userRouter = require('./user');
+const loginRouter = require('./login');
 
 const root = express.Router({ mergeParams: true });
 
 root.use('/user', userRouter);
+root.use('/login', loginRouter);
 
 module.exports = root;
