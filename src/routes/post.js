080f996b4c6a3f8ccf89f465ea81ea05dafdc31d
Roberval Filho <rslfilho@gmail.com>
2022-03-13 18:24:18 -0300

    feat : adicionando rota GET/post/search

--- src/routes/post.js
+++ src/routes/post.js
@@ -7,6 +7,7 @@ const router = express.Router({ mergeParams: true });
 
 router.post('/', auth, validatePost, postController.create);
 router.get('/', auth, postController.getAll);
+router.get('/search', auth, postController.search);
 router.get('/:id', auth, postController.getById);
 router.delete('/:id', auth, postController.remove);
 router.put('/:id', auth, validatePost, postController.update);
