547a4488b11aa0a9a96a9ba1a07ab188ca212879
Roberval Filho <rslfilho@gmail.com>
2022-03-13 06:22:28 -0300

    test : testes de integração da rota DELETE/user/me

---
+++ src/tests/02-integration/user/deleteUserMe.test.js
@@ -0,0 +1,113 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+
+const app = require('../../../api/app');
+const { User } = require('../../../database/models');
+const { jwt } = require('../../../helpers');
+const userMock = require('../../mocks/user');
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe.only('DELETE/user/me', () => {
+  describe('Em caso de erro interno', () => {
+    let response;
+    before(async () => {
+      sinon.stub(jwt, 'validateToken').returns(userMock.created)
+      sinon.stub(User, 'destroy').rejects();
+      response = await chai.request(app)
+        .delete('/user/me')
+        .set('Authorization', userMock.token)
+
+      await jwt.validateToken.restore();
+      await User.destroy.restore();
+    });
+
+    it('retorna o código de status 500', () => {
+      expect(response).to.have.status(500);
+    });
+
+    it('retorna um objeto', () => {
+      expect(response.body).to.be.an('object');
+    });
+
+    it('o objeto possui a propriedade "message"', () => {
+      expect(response.body).to.have.property('message');
+    });
+
+    it('a propriedade "message" possui o texto "Internal Error"', () => {
+      expect(response.body.message).to.be.equal('Internal Error');
+    });
+  });
+
+  describe('Não é possível deletar usuários', () => {
+    describe('com token de autenticação ausente', () => {
+      let response;
+      before(async () => {
+        response = await chai.request(app)
+          .delete('/user/me')
+      });
+
+      it('retorna o código de status 401', () => {
+        expect(response).to.have.status(401);
+      });
+
+      it('retorna um objeto', () => {
+        expect(response.body).to.be.an('object');
+      });
+
+      it('o objeto possui a propriedade "message"', () => {
+        expect(response.body).to.have.property('message');
+      });
+
+      it('a propriedade "message" possui o texto "Token não encontrado"', () => {
+        expect(response.body.message).to.be.equal('Token não encontrado');
+      });
+    });
+
+    describe('com um token de autenticação inválido ou expirado', () => {
+      let response;
+      before(async () => {
+        response = await chai.request(app)
+          .delete('/user/me')
+          .set('Authorization', 'tokenIvalido')
+      });
+
+      it('retorna o código de status 401', () => {
+        expect(response).to.have.status(401);
+      });
+
+      it('retorna um objeto', () => {
+        expect(response.body).to.be.an('object');
+      });
+
+      it('o objeto possui a propriedade "message"', () => {
+        expect(response.body).to.have.property('message');
+      });
+
+      it('a propriedade "message" possui o texto "Token expirado ou inválido"', () => {
+        expect(response.body.message).to.be.equal('Token expirado ou inválido');
+      });
+    });
+  });
+
+  describe('É possível deletar usuários com token válido', () => {
+    let response;
+      before(async () => {
+        sinon.stub(jwt, 'validateToken').returns(userMock.created)
+        sinon.stub(User, 'destroy').resolves(undefined);
+        response = await chai.request(app)
+        .delete('/user/me')
+        .set('Authorization', userMock.token)
+
+      await jwt.validateToken.restore();
+      await User.destroy.restore();
+      });
+      
+      it('retorna o código de status 204', () => {
+        expect(response).to.have.status(204);
+      });
+  });
+});
